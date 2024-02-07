390554163f3d5f244162bf689258a105d7ab165a
asouzh <andrin.soppelsa@uzh.ch>
2024-02-07 14:41:46 +0100

    commit

--- test.R
+++ test.R
@@ -1,1 +1,15 @@
 #test
+
+
+# git init
+
+# git add .
+
+# git commit -m "first commit"
+
+# git config --global user.email "andrin.soppelsa@uzh.ch"
+# git config --global user.name "asouzh"
+
+# git remote add origin GitHub URL
+
+# git push -u origin main
